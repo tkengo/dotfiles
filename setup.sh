0e49b7669554c80369d73350c41fc7c2a39afc2a
tkengo <tkengo@kengo.local.home>
2012-05-02 22:32:59 +0900

    シンボリックリンクをはるシェルを追加

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+# シンボリックリンクをはるファイル、ディレクトリのリスト
+GITLIST="
+.vim
+.vimrc
+.zshrc
+"
+
+# リストの分だけ繰り返してシンボリックリンクをはる
+for FILE in ${GITLIST};
+do
+	rm -fr ~/${FILE}
+	ln -s ~/.dotfiles/${FILE} ~/${FILE}
+done
+
+# Vundle更新
+cd ~/.dotfiles
+git submodule init
+git submodule update
