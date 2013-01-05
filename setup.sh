7ec4b6b41411555e168a16bcf3178d616a74c421
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2013-01-05 16:40:14 +0900

    binディレクトリのスクリプトに実行権限を付与するようにした

--- setup.sh
+++ setup.sh
@@ -11,7 +11,13 @@ SYMLINK_LIST="
 
 # セットアップスクリプトがあるパスの取得
 DOTFILES_PATH=$(cd $(dirname $0);pwd)
+
+# binディレクトリのファイルに実行権限付与
 BIN_PATH=$DOTFILES_PATH/bin
+BIN_LIST=`ls $BIN_PATH`
+for FILE in $BIN_LIST; do
+    chmod +x $BIN_PATH/$FILE
+done
 
 # リストの分だけ繰り返してシンボリックリンクをはる
 for FILE in $SYMLINK_LIST; do
