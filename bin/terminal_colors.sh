3bfc55d06c262831dec6b132f15300de401fe8c1
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2013-01-26 23:01:11 +0900

    8色ターミナルカラーも出力できるように

--- bin/terminal_colors.sh
+++ bin/terminal_colors.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for i in {0..255}; do
-    printf "\x1b[38;5;${i}mcolour%03d " $i
-    [ $(((i+1) % 10)) -eq 0 ] && printf "\n"
-done
+if [ "$1" = "8" ]; then
+    printf "\e[30mblack\e[m"
+    printf " \e[31mred\e[m"
+    printf " \e[32mgreen\e[m"
+    printf " \e[33myellow\e[m"
+    printf " \e[34mblue\e[m"
+    printf " \e[35mmagenta\e[m"
+    printf " \e[36mcyan\e[m"
+    printf " \e[37mwhite\e[m"
+else
+    for i in {0..255}; do
+        printf "\x1b[38;5;${i}mcolour%03d " $i
+        [ $(((i+1) % 10)) -eq 0 ] && printf "\n"
+    done
+fi
