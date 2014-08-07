125efeeb6a65912b9687c714d6001cd3721c041c
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-08-07 16:13:08 +0900

    setup.sh内で全部完結できそう

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ SYMLINK_LIST="
 
 # セットアップスクリプトがあるパスの取得
 BASE_DIR=$(cd $(dirname $0);pwd)
+cd $BASE_DIR
 
 # リストの分だけ繰り返してシンボリックリンクをはる
 for FILE in $SYMLINK_LIST; do
@@ -21,10 +22,41 @@ for FILE in $SYMLINK_LIST; do
 	ln -s $BASE_DIR/$FILE ~/$FILE
 done
 
+if [ `uname` != 'Darwin' ]; then
+    # homebrewのインストール
+    which brew > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    fi
+
+    # Brewfileをもとに全部インストール
+    brew bundle
+else
+    sudo yum install git tig tmux zsh direnv global
+fi
+
+# zshをログインシェルに変更
+ZSH_PATH=$(which zsh)
+SHELLS_PATH=/etc/shells
+if [ -z "`cat $SHELLS_PATH | grep $ZSH_PATH`" ]; then
+    sudo sh -c "echo $ZSH_PATH >> $SHELLS_PATH"
+fi
+if [ $SHELL != $ZSH_PATH ]; then
+    chsh -s $ZSH_PATH
+fi
+
+# rbenvのインストール
+if [ ! -d ~/.rbenv ]; then
+    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+fi
+
+# ruby-buildのインストール
+if [ ! -d ~/.rbenv/plugins/ruby-build ]; then
+    git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+
 # Vundle更新
-cd $BASE_DIR
 git submodule init
 git submodule update
-
 
 echo Complete setup.sh
--- setup/install_autojump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 既にインストール済みであれば何もしない
-[ -d ~/.autojump ] && exit 0
-
-# autojumpインストール
-cd `dirname $0`
-git clone git://github.com/joelthelion/autojump.git
-cd autojump && chmod +x install.sh && ./install.sh && cd .. && rm -fr autojump
