9a8f2d0c5e88e11f882a52fb3c1d96ac1996df77
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2015-09-25 07:29:12 +0000

    Ubuntu用のセットアップスクリプトを更新

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# シンボリックリンクをはるファイル、ディレクトリのリスト
+# List of symlinks.
 SYMLINK_LIST="
 .vim
 .vimrc
@@ -14,33 +14,36 @@ SYMLINK_LIST="
 .gemrc
 "
 
-# セットアップスクリプトがあるパスの取得
+# Get the path of this script, and move there.
 BASE_DIR=$(cd $(dirname $0);pwd)
 cd $BASE_DIR
 
-# リストの分だけ繰り返してシンボリックリンクをはる
+# Symlinks.
 for FILE in $SYMLINK_LIST; do
     rm -fr ~/$FILE
     ln -s $BASE_DIR/$FILE ~/$FILE
 done
 
 if [ `uname` == 'Darwin' ]; then
-    # homebrewのインストール
+    # For Mac OS X.
+    # Install homebrew.
     which brew > /dev/null 2>&1
     if [ $? -eq 1 ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     fi
 
-    # Brewfileをもとに全部インストール
     brew bundle
+elif [[ -f /etc/debian_version ]]; then
+    # For Debian destribution.
+    sudo apt-get install -y vim tig tmux zsh global ctags
 else
+    # Any other Linux destribution.
     sudo rpm -ivh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
     sudo rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
     sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-    sudo yum install -y vim tig tmux zsh direnv global --enablerepo=rpmforge
+    sudo yum install -y vim tig tmux zsh direnv global ctags --enablerepo=rpmforge
 fi
 
-# Vundle更新
 git submodule init
 git submodule update
 
