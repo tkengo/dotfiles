b14a4ad7ab7f6e133dc10492a3cc60a3863cf62b
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2018-03-11 10:23:11 +0900

    パッケージインストールのスキップオプションを追加

--- setup.sh
+++ setup.sh
@@ -25,20 +25,24 @@ for FILE in $SYMLINK_LIST; do
     ln -s $BASE_DIR/$FILE ~/$FILE
 done
 
-if [ `uname` == 'Darwin' ]; then
-    # Do Nothing
-    echo Darwing Setup
-elif [[ -f /etc/debian_version ]]; then
-    # For Debian destribution.
-    sudo apt-get install -y vim tig tmux zsh global ctags
-else
-    # Any other Linux destribution.
-    sudo rpm -ivh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-    sudo yum install -y vim tig tmux zsh direnv global ctags
+if [ "$1" != '--skip-package-install' ]; then
+    if [ `uname` == 'Darwin' ]; then
+        # Do Nothing
+        echo Darwing Setup
+    elif [[ -f /etc/debian_version ]]; then
+        # For Debian destribution.
+        sudo apt-get install -y vim tig tmux zsh global ctags
+    else
+        # Any other Linux destribution.
+        sudo rpm -ivh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+        sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+        sudo yum install -y vim tig tmux zsh direnv global ctags
+    fi
 fi
 
 git submodule init
 git submodule update
+
+vim +:BundleInstall +:qa
 
 echo Complete setup.sh
